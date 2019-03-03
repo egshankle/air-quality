9691deb9d626cd865f9977b0d42f478bf265a6ab
beth <1774902+egshankle@users.noreply.github.com>
2019-03-03 13:43:35 -0500

    now with sensors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Leaderboard from "./Leaderboard";
+import Sensors from "./Sensors";
 import './App.css';
 
 class App extends Component {
@@ -11,8 +12,10 @@ class App extends Component {
           <div>
             <Link to="/">Home</Link>
             <Link to="/leaderboard">Leaderboard</Link>
+            <Link to="/sensors">Sensors</Link>
             <Route exact path="/" component={Home} />
             <Route path="/leaderboard" component={Leaderboard} />
+            <Route path="/sensors" component={Sensors} />
           </div>
         </Router>
     );
---
+++ src/Sensors.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Sensors extends React.Component {
+    render() {
+        return (
+                <div>Sensors</div>
+        );
+    }
+}
+
+export default Sensors;
